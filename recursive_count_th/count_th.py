95cd965c2e45870a7d7c149c80409046103e59fb
alexis villaraza <alexis.borja@gmail.com>
2020-06-19 08:42:58 -0700

    finished short answer

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,6 +5,19 @@ Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
     
-    # TBC
+    print("count_th", word)
+    count = 0
+
+    # basecase
+    if len(word) <= 0:
+        return len(word)
+       
+    if word[0:2] == 'th':
+        print('this word', word[0:2])
+        print(word[1:3])
+        count = 1
+
+    count += count_th(word[1:len(word)])
+    return count
     
-    pass
+    
